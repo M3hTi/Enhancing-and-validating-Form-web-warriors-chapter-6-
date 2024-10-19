0037ab1a4628e7ccd5847a8d3699ef92eb001466
M3hTi <mehdinasir502@yahoo.com>
2024-10-19 17:14:34 +0330

    finish calcOrder function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const run = function () {
+const initializeProductOrderForm = function () {
     const productOrder = document.forms.productOrder
     // console.log(productOrder);
     const chooseModel = productOrder.elements.model
@@ -7,31 +7,68 @@ const run = function () {
     const selectionList = productOrder.querySelectorAll('select')
     //  console.log(selectionList);
 
+    const optionRadioElements = productOrder.querySelectorAll('input[type= "radio"]')
+    // console.log(optionRadioElements);
+
     chooseModel.focus()
 
     calcOrder()
 
     function calcOrder(){
-        let modelIndex = chooseModel.selectedIndex 
+        const modelIndex = chooseModel.selectedIndex 
         // console.log(modelIndex);
-        let modelPrice = parseFloat(chooseModel.options[modelIndex].value)
+        const modelPrice = parseFloat(chooseModel.options[modelIndex].value)
         
         
 
         const quantitySelection = productOrder.elements.quantity
         // console.log(quantity);
-        let quantityIndex = quantitySelection.selectedIndex
-        let quantity = parseInt(quantitySelection.options[quantityIndex].value)
+        const quantityIndex = quantitySelection.selectedIndex
+        const quantity = parseInt(quantitySelection.options[quantityIndex].value)
 
 
-        let modelCost = productOrder.elements.modelCost
-        modelCost.value = (modelPrice * quantity).toFixed(2)
+        const modelCost = productOrder.elements.modelCost
+        const modelTotal = modelPrice * quantity;
+        modelCost.value = modelTotal.toLocaleString(undefined, {style: 'currency', currency: 'USD'});
 
 
+        //NOTE: retrieve the cost of the protection plan
+        const plan = parseFloat(document.querySelector('input[name = "plan"]:checked').value) 
+        
+        // NOTE: charge the plan to each item ordered
+        const planCost = productOrder.elements.planCost
+        // console.log(planCost);
+
+        const planTotal = plan * quantity;
+        planCost.value = planTotal.toLocaleString(undefined, {style: 'currency', currency: 'USD'});
+
+
+        // NOTE: Calculate the order subtotal
+        const subTotal = productOrder.elements.subTotal
+        
+        const subTotalValue = modelTotal + planTotal;
+        subTotal.value = `${(subTotalValue).toLocaleString(undefined, {style: 'currency', currency : 'USD'})}`
+
+
+
+
+        // NOTE: Calculate the 5% sales tax
+        const salesTax = productOrder.elements.salesTax
+        const taxAmount = subTotalValue * 0.05;
+        salesTax.value = taxAmount.toLocaleString(undefined, {style: 'currency', currency: 'USD'});
+
+
+        //NOTE: Calculate the total cost of the order
+        const totalCost = productOrder.elements.totalCost 
+        totalCost.value = (subTotalValue + taxAmount).toLocaleString(undefined, {style: 'currency', currency: 'USD'});
     }
 
     selectionList.forEach( select => {
         select.addEventListener('change', calcOrder)
+    });
+
+    optionRadioElements.forEach( option => {
+        option.addEventListener('change', calcOrder)
     });
     
     
@@ -52,7 +89,7 @@ const run = function () {
 
 
 
-window.addEventListener('load', run)
+window.addEventListener('load', initializeProductOrderForm)
 
 
 
